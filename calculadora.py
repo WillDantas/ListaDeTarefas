e9dec5fbb8a94b0d0c9422297d6ffc16b9ddc91a
William Dantas <william.dantassp1@gmail.com>
2025-09-30 21:00:17 -0300

    Criação de loopings e variaveis

--- calculadora.py
+++ calculadora.py
@@ -1,2 +1,9 @@
+num1 = 0
+num2 = 0
+resultado = 0
+op = ''
 
- 
+while True:
+    num1 = float(input('Digite o primeiro numero:: '))
+    op = input('Digite a operação matematica a ser feita: ')
+    num2 = float(input('Digite o segundo numero:: '))
