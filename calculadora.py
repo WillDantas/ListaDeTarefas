7ae24833186c7ed6675eb95567150f9cc8b245f3
William Dantas <william.dantassp1@gmail.com>
2025-09-30 21:01:04 -0300

    Criação das operações

--- calculadora.py
+++ calculadora.py
@@ -7,3 +7,14 @@ while True:
     num1 = float(input('Digite o primeiro numero:: '))
     op = input('Digite a operação matematica a ser feita: ')
     num2 = float(input('Digite o segundo numero:: '))
+
+    if op == '+':
+        resultado = num1 + num2
+    elif op == '-':
+        resultado = num1 - num2
+    elif op == '/':
+        resultado = num1 / num2
+    elif op == '*':
+        resultado = num1 * num2
+    else:
+        print('Operação não reconhecida!')
